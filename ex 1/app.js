2c1ebf1281ecb527ce0d5dce04ff103d888ec216
MaximeMartrat <maximemartrat@outlook.fr>
2023-01-30 11:17:06 +0100

    maj app.js

--- ex 1/app.js
+++ ex 1/app.js
@@ -1,1 +1,18 @@
-document.cookie = "username=TOTO"
+//let mydate = new Date(Date.UTC(2023, 11, 31, 20, 00, 0));
+let today = new Date();
+let time = today.getTime();
+let expireTime = time + (2*24*60*60*1000);
+today.setTime(expireTime);
+document.cookie = `username=TOTO;expires=${today.toUTCString()};path=/;priority=high`;
+//recupérer les valeurs d'un cookie
+let myC = document.cookie ;
+console.log(myC.split('=')[1]);
+
+//modifier un cookie
+document.cookie = `username=TUTU;expires=${today.toUTCString()};path=/;priority=high`;
+
+//suprimer un cookie
+//document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+//ajouter des champs dans le cookie
+document.cookie = "birthday=281080"
